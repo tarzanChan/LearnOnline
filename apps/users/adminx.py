86329ced20c51bc4eca483d810f1dbcc91ae1563
Palameng <506357902@qq.com>
2017-07-23 19:52:39 +0800

    2017/7/23 add xadmin

---
+++ apps/users/adminx.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+__author__ = 'mengyuan'
+__date__ = '2017/7/23 17:12'
+
+import xadmin
+from .models import EmailVerifyRecord, Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']     # 告诉后台要在哪些字段中搜索
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']     # 告诉后台要在哪些字段中搜索
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
