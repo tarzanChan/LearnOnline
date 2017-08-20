d67fe4878916f28923af46ce5d9e455a007bf07a
Palameng <506357902@qq.com>
2017-08-20 14:37:42 +0800

    1. 使用django-pure-pagination库完成分页功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import View
 from .models import CourseOrg, CityDict
 
@@ -16,8 +17,23 @@ class OrgView(View):
         all_cities = CityDict.objects.all()
 
         org_nums = all_orgs.count()
+
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        objects = ['john', 'edward', 'josh', 'frank']
+
+        # Provide Paginator with the request object for complete querystring generation
+        # 第二个参数代表每一页显示的个数
+        p = Paginator(all_orgs, 2, request=request)
+
+        orgs = p.page(page)
+
         return render(request, "org-list.html", {
-            "all_orgs": all_orgs,
+            "all_orgs": orgs,
             "all_cities": all_cities,
             "org_nums": org_nums,
         })
