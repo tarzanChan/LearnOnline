ee6e7988b06477934301656b803ec96e30599f4f
Palameng <506357902@qq.com>
2017-08-20 13:45:59 +0800

    1.处理了一些列表展示;

--- Mxonline/urls.py
+++ Mxonline/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
+from django.views.static import serve
 from users.views import LoginView, RegisterView, ActiveUserView, ForgetView, ResetPasswordView, ModifyPasswordView
 from organization.views import OrgView
+from Mxonline.settings import MEDIA_ROOT
 import xadmin
 
 
@@ -35,4 +37,7 @@ urlpatterns = [
 
     # 课程机构首页
     url(r'^org_list/$', OrgView.as_view(), name="org_list"),
+
+    # 配置上传文件的访问处理函数
+    url(r'^media/(?P<path>.*)', serve, {"document_root": MEDIA_ROOT}),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import View
-
+from .models import CourseOrg, CityDict
 
 class OrgView(View):
     """
     课程机构列表功能
     """
     def get(self, request):
-        return render(request, "org-list.html", {})
+        # 课程机构
+        all_orgs = CourseOrg.objects.all()
+        # 城市
+        all_cities = CityDict.objects.all()
+
+        org_nums = all_orgs.count()
+        return render(request, "org-list.html", {
+            "all_orgs": all_orgs,
+            "all_cities": all_cities,
+            "org_nums": org_nums,
+        })
