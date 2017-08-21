afa477f714f9b0cc9ec11d88ec4faf09ed290d8d
Palameng <506357902@qq.com>
2017-08-21 22:11:59 +0800

    add function: filter data

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -13,10 +13,29 @@ class OrgView(View):
     def get(self, request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
+
         # 城市
         all_cities = CityDict.objects.all()
 
+        # 取出筛选城市
+        # (这里因为city属于CourseOrg的一个外键，在数据库中存储的是city_id,
+        # 所以这里filter_city得到的是一个字符串类型的id,而不是一个city类实例)
+        filter_city = request.GET.get('city', "")
+        if filter_city:
+            all_orgs = all_orgs.filter(city_id=int(filter_city))
+
+        # 类别筛选
+        category = request.GET.get('ct', "")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        # 机构总数
         org_nums = all_orgs.count()
+
+
+        #取出热门课程排序
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
+
 
         # 对课程机构进行分页
         try:
@@ -24,16 +43,16 @@ class OrgView(View):
         except PageNotAnInteger:
             page = 1
 
-        objects = ['john', 'edward', 'josh', 'frank']
-
         # Provide Paginator with the request object for complete querystring generation
         # 第二个参数代表每一页显示的个数
         p = Paginator(all_orgs, 2, request=request)
-
         orgs = p.page(page)
 
         return render(request, "org-list.html", {
             "all_orgs": orgs,
             "all_cities": all_cities,
             "org_nums": org_nums,
+            "filter_city": filter_city,
+            "category": category,
+            "hot_orgs": hot_orgs,
         })
