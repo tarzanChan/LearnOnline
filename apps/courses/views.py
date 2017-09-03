995caefba516c850b1de8f0a4578dacfbff82a8b
Palameng <506357902@qq.com>
2017-09-03 10:28:02 +0800

    完善了收藏按钮功能

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -4,6 +4,7 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 from django.views.generic import View
 from .models import Course
+from operation.models import UserFavorite
 
 
 class CourseListView(View):
@@ -45,6 +46,16 @@ class CourseDetailView(View):
         course.click_nums += 1
         course.save()
 
+        has_fav_course = False
+        has_fav_org = False
+
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
+                has_fav_course = True
+
+            elif UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
+
         tag = course.tag
         if tag:
             relate_courses = Course.objects.filter(tag=tag)[:1]
@@ -54,4 +65,6 @@ class CourseDetailView(View):
         return render(request, 'course-detail.html', {
             "course": course,
             "relate_courses": relate_courses,
+            "has_fav_course": has_fav_course,
+            "has_fav_org": has_fav_org,
         })
